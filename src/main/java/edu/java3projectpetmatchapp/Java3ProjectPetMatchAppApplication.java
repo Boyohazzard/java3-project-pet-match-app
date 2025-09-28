c53778cfe80375a8da4edb06cabd005bd8c7ffb7
GlaabJ <jonglaab@gmail.com>
2025-09-28 19:25:18 -0400

    first post

---
+++ src/main/java/edu/java3projectpetmatchapp/Java3ProjectPetMatchAppApplication.java
@@ -0,0 +1,13 @@
+package edu.java3projectpetmatchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java3ProjectPetMatchAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java3ProjectPetMatchAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/java3projectpetmatchapp/Java3ProjectPetMatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java3projectpetmatchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java3ProjectPetMatchAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
