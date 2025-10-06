e1e9d365d35c7552be167996540ede6fc44e8c96
ElizabethMokrusa <elimokrusa@msn.com>
2025-10-06 09:56:44 -0400

    Added admin dashboard template and link to view it when logged in as ADMIN.

---
+++ src/main/java/edu/java3projectpetmatchapp/controller/AdminController.java
@@ -0,0 +1,30 @@
+package edu.java3projectpetmatchapp.controller;
+
+import edu.java3projectpetmatchapp.entity.User;
+import edu.java3projectpetmatchapp.service.CustomUserDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final CustonUserDetailsService userService;
+
+    public AdminController(CustomUserDetailsService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/dashboard")
+    public String showAdminDashboard(Model model) {
+
+        List<User> users = userService.getAllUsers();
+
+        model.addAttribute("users", users);
+        return "admin_dashboard";
+    }
+}
